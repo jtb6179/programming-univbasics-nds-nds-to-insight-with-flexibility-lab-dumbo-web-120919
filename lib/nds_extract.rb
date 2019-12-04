1f726e02a215981b0ba4050ba08dc78f1c1abcbd
Joel  Bly <jtb6179@g.rit.edu>
2019-12-04 23:47:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -56,8 +56,9 @@ def movies_with_director_key(name, movies_collection)
   tup = movies_with_director_key(name, movies_collection)
   while ty < movies_collection.length do 
     quip = movies_collection[counter][:title]
-    stuff = (quip) 
  binding.pry   
+ stuff = (quip) 
+    
  empty_hash << stuff
   
   ty += 1 
