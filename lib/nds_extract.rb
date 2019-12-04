6e5f19bef59231e592e491ed7ae1d89d2d63e066
Joel  Bly <jtb6179@g.rit.edu>
2019-12-04 23:10:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -55,7 +55,7 @@ def movies_with_director_key(name, movies_collection)
   
   while ty < movies_collection.length do 
     quip = movies_collection[counter][:title]
-    qool = 
+    
   
   binding.pry 
   
